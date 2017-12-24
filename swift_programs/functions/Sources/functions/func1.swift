483bc674479c5c437410347a0aeaa338ed54c463
Rajat Jaic Mendus <jaicmendus@gmail.com>
2017-12-24 10:59:13 +0530

    Small changes made to print statements

--- swift_programs/functions/Sources/functions/func1.swift
+++ swift_programs/functions/Sources/functions/func1.swift
@@ -1,7 +1,6 @@
 func getMilk() {
-    let myName = "Rajat"
     print("Go to the shop")
     print("Buy 2 cartons of milk")
     print("Pay $2")
-    print("Go home \(myName)")
+    print("Go home")
 }
--- swift_programs/functions/Sources/functions/func2.swift
+++ swift_programs/functions/Sources/functions/func2.swift
@@ -1,8 +1,7 @@
 func getMilkWithInp(howManyMilkCartons : Int) {
-    let myName = "Rajat"
     print("Go to the shop")
     print("Buy \(howManyMilkCartons) cartons of milk")
     let priceToPay = howManyMilkCartons * 2
     print("Pay \(priceToPay)")
-    print("Go home \(myName)")
+    print("Go home")
 }
