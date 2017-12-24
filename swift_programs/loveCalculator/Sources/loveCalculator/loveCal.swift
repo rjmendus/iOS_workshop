07d1add0e370b318adde68b1fa0304df077cffd2
Rajat Jaic Mendus <jaicmendus@gmail.com>
2017-12-24 11:50:47 +0530

    Upgraded love calculator with 3 outputs

--- swift_programs/loveCalculator/Sources/loveCalculator/loveCal.swift
+++ swift_programs/loveCalculator/Sources/loveCalculator/loveCal.swift
@@ -1,5 +1,15 @@
-func loveCalculator(yourName : String, theirName : String) -> Int {
+func loveCalculator(yourName : String, theirName : String) -> String {
+
     let loveScore = getRandom(upperBound : 101)
 
-    return loveScore
+    if loveScore > 80 {
+        return "Your love score is \(loveScore). You love each other like Kanye loves Kanye!"
+    }
+    else if loveScore > 40 && loveScore <= 80 {
+        return "Your love score is \(loveScore). You go together like coke and mentos!"
+    }
+    else {
+        return "Your love score is \(loveScore). No love possible, forever alone!"
+    }
+
 }
