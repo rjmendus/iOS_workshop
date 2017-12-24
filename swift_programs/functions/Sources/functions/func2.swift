92d57e12fc24e00895769da9729e6be6430303a1
Rajat Jaic Mendus <jaicmendus@gmail.com>
2017-12-24 10:47:26 +0530

    Function modified to obtain inputs

---
+++ swift_programs/functions/Sources/functions/func2.swift
@@ -0,0 +1,8 @@
+func getMilkWithInp(howManyMilkCartons : Int) {
+    let myName = "Rajat"
+    print("Go to the shop")
+    print("Buy \(howManyMilkCartons) cartons of milk")
+    let priceToPay = howManyMilkCartons * 2
+    print("Pay \(priceToPay)")
+    print("Go home \(myName)")
+}
--- swift_programs/functions/Sources/functions/main.swift
+++ swift_programs/functions/Sources/functions/main.swift
@@ -1,1 +1,8 @@
+print("Simple getMilk function with no inputs")
 getMilk()
+
+print("")
+print("")
+
+print("getMilk function with inpu")
+getMilkWithInp(howManyMilkCartons :4)
