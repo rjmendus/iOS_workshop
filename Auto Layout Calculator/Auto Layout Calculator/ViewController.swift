b6440dd343706ccedaf573ea1da802e42a07bce6
Rajat Jaic Mendus <jaicmendus@gmail.com>
2017-12-31 16:51:02 +0530

    Calculator UI made using stack view

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by user133729 on 12/31/17.
+//  Copyright © 2017 rjmendus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+    
+}
+
---
+++ Auto Layout Calculator/Auto Layout CalculatorTests/Auto_Layout_CalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Auto_Layout_CalculatorTests.swift
+//  Auto Layout CalculatorTests
+//
+//  Created by user133729 on 12/31/17.
+//  Copyright © 2017 rjmendus. All rights reserved.
+//
+
+import XCTest
+@testable import Auto_Layout_Calculator
+
+class Auto_Layout_CalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
